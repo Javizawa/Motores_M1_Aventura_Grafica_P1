a22958535d4260a1285e8a4452830e8d6833aaf9
Javizawa <84777203+Javizawa@users.noreply.github.com>
2024-10-21 12:22:24 +0200

    First Commit

---
+++ Assets/Scripts/ButtonsBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TEST");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LookButton()
+    {
+        Debug.Log("Has pulsado el botón Mirar");
+    }
+
+
+}
